f1652f072658fad8b9511df418a4b9642f0a8124
galaxyyvfx <ksgalaxyy@gmail.com>
2023-09-25 17:15:14 +0700

    Added Project class & DbContext

---
+++ BusinessObject/Models/IdtDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BusinessObject.Models;
+
+public class IdtDbContext : DbContext
+{
+    public IdtDbContext() : base()
+    {
+    }
+
+    public DbSet<Project> Projects { get; set; } = default!;
+}
---
+++ BusinessObject/Models/Project.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessObject.Models;
+
+public class Project
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = default!;
+
+    [Required]
+    public string CompanyName { get; set; } = default!;
+
+    [Required]
+    public string Location { get; set; } = default!;
+
+    public string? Description { get; set; }
+
+    [Required]
+    public DateTime CreatedDate { get; set; }
+
+    [Required]
+    public DateTime UpdatedDate { get; set; }
+
+    [Required]
+    public int NoStage { get; set; }
+
+    public double EstimatedPrice { get; set; }
+
+    public double FinalPrice { get; set; }
+
+    [Required]
+    public int Language { get; set; }
+
+    [Required]
+    public bool IsAdvertisement { get; set; }
+
+    [Required]
+    public int Status { get; set; }
+}
