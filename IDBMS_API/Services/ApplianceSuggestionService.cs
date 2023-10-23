e91eb431057b2f5604d440945e1ded8062635b95
Slacknwork <tphcmtrung@gmail.com>
2023-10-23 23:32:19 +0700

    Add ApplianceSuggestion, Comment, PrepayStage, Transaction service

---
+++ IDBMS_API/Services/ApplianceSuggestionService.cs
@@ -0,0 +1,48 @@
+﻿using BusinessObject.DTOs.Request.CreateRequests;
+using BusinessObject.Models;
+using Repository.Interfaces;
+
+namespace IDBMS_API.Services
+{
+    public class ApplianceSuggestionService
+    {
+        private readonly IApplianceSuggestionRepository _repository;
+        public ApplianceSuggestionService(IApplianceSuggestionRepository repository)
+        {
+            _repository = repository;
+        }
+        public IEnumerable<ApplianceSuggestion> GetAll()
+        {
+            return _repository.GetAll();
+        }
+        public ApplianceSuggestion? GetById(Guid id)
+        {
+            return _repository.GetById(id);
+        }
+        public ApplianceSuggestion? CreateApplianceSuggestion(ApplianceSuggestionRequest request)
+        {
+            var applianceSuggestion = new ApplianceSuggestion
+            {
+                Name = request.Name,
+                Description = request.Description,
+                ImageUrl = request.ImageUrl,
+                InteriorItemId = request.InteriorItemId,
+                RoomId = request.RoomId,
+            };
+            var asCreated = _repository.Save(applianceSuggestion);
+            return asCreated;
+        }
+        public void UpdateApplianceSuggestion(ApplianceSuggestionRequest request)
+        {
+            var applianceSuggestion = new ApplianceSuggestion
+            {
+                Name = request.Name,
+                Description = request.Description,
+                ImageUrl = request.ImageUrl,
+                InteriorItemId = request.InteriorItemId,
+                RoomId = request.RoomId,
+            };
+            _repository.Update(applianceSuggestion);
+        }
+    }
+}
