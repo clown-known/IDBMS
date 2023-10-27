0b76ce0bcc9e4427ab1f38c7c090a92b605a5647
tkiet1808 <kietpttse161220@fpt.edu.vn>
2023-10-27 21:53:57 +0700

    add prepay stage controller

--- IDBMS_API/Controllers/IDBMSControllers/PrepayStageController.cs
+++ IDBMS_API/Controllers/IDBMSControllers/PrepayStageController.cs
@@ -1,14 +1,82 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessObject.DTOs.Request;
+using IDBMS_API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
 
 namespace IDBMS_API.Controllers.IDBMSControllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PrepayStageController : Controller
+    public class PrepayStageController : ODataController
     {
-        public IActionResult Index()
+        private readonly PrepayStageService _service;
+
+        public PrepayStageController(PrepayStageService service)
         {
-            return View();
+            _service = service;
+        }
+
+        [EnableQuery]
+        [HttpGet]
+        public IActionResult GetPrepayStages()
+        {
+            return Ok(_service.GetAll());
+        }
+        //all
+        [EnableQuery]
+        [HttpGet("project/{projectId}")]
+        public IActionResult GetPrepayStagesByProjectId(Guid projectId)
+        {
+            return Ok(_service.GetByProjectId(projectId));
+        }
+        //permission
+        [EnableQuery]
+        [HttpGet("{id}")]
+        public IActionResult GetPrepayStagesById(Guid id)
+        {
+            return Ok(_service.GetById(id));
+        }
+        [HttpPost]
+        public IActionResult CreatePrepayStage([FromBody] PrepayStageRequest request)
+        {
+            try
+            {
+                _service.CreatePrepayStage(request);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdatePrepayStage(Guid id, [FromBody] PrepayStageRequest request)
+        {
+            try
+            {
+                _service.UpdatePrepayStage(id, request);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return Ok();
+        }
+
+        [HttpPut("{id}/isHidden")]
+        public IActionResult UpdatePrepayStageStatus(Guid id, bool isHidden)
+        {
+            try
+            {
+                _service.UpdatePrepayStageStatus(id, isHidden);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return Ok();
         }
     }
 }
