ef39b26614dd28e46388adfbf45c0e0ff4d342f6
Slacknwork <tphcmtrung@gmail.com>
2023-11-22 01:09:25 +0700

    Add missing Controller and Service

--- IDBMS_API/Controllers/IDBMSControllers/SiteController.cs
+++ IDBMS_API/Controllers/IDBMSControllers/SiteController.cs
@@ -1,12 +1,100 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessObject.DTOs.Request;
+using BusinessObject.DTOs.Response;
+using IDBMS_API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
 
 namespace IDBMS_API.Controllers.IDBMSControllers
 {
-    public class SiteController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SitesController : ODataController
     {
-        public IActionResult Index()
+        private readonly SiteService _service;
+
+        public SitesController(SiteService service)
         {
-            return View();
+            _service = service;
+        }
+
+        [EnableQuery]
+        [HttpGet]
+        public IActionResult GetSites()
+        {
+            var response = new ResponseMessage()
+            {
+                Message = "Get successfully!",
+                Data = _service.GetAll()
+            };
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public IActionResult CreateSite([FromBody] SiteRequest request)
+        {
+                try
+                {
+                    var result = _service.CreateSite(request);
+                    var response = new ResponseMessage()
+                    {
+                        Message = "Create successfully!",
+                        Data = result
+                    };
+                    return Ok(response);
+                }
+                catch (Exception ex)
+                {
+                    var response = new ResponseMessage()
+                    {
+                        Message = $"Error: {ex.Message}"
+                    };
+                    return BadRequest(response);
+                }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateSite(Guid id, [FromBody] SiteRequest request)
+        {
+            try
+            {
+                _service.UpdateSite(id, request);
+                var response = new ResponseMessage()
+                {
+                    Message = "Update successfully!",
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                var response = new ResponseMessage()
+                {
+                    Message = $"Error: {ex.Message}"
+                };
+                return BadRequest(response);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteSite(Guid id)
+        {
+            try
+            {
+                _service.DeleteSite(id);
+                var response = new ResponseMessage()
+                {
+                    Message = "Delete successfully!",
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                var response = new ResponseMessage()
+                {
+                    Message = $"Error: {ex.Message}"
+                };
+                return BadRequest(response);
+            }
         }
     }
 }
