f46b43664675e9ac3fcec2d69e405c3868be4171
tkiet1808 <kietpttse161220@fpt.edu.vn>
2023-12-14 12:59:57 +0700

    update get participation by project id

--- Repository/Interfaces/IProjectParticipationRepository.cs
+++ Repository/Interfaces/IProjectParticipationRepository.cs
@@ -6,5 +6,8 @@ namespace Repository.Interfaces;
 public interface IProjectParticipationRepository : ICrudBaseRepository<ProjectParticipation, Guid>
 {
     IEnumerable<ProjectParticipation> GetByProjectId(Guid id);
+    IEnumerable<ProjectParticipation> GetProjectMemberByProjectId(Guid id);
     IEnumerable<ProjectParticipation> GetByUserId(Guid id);
+    ProjectParticipation? GetProjectOwnerByProjectId(Guid id);
+    ProjectParticipation? GetProjectManagerByProjectId(Guid id);
 }
