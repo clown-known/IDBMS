0aae09e88f39adedf8a16b07f2fc76c2d9ccee4a
tkiet1808 <kietpttse161220@fpt.edu.vn>
2023-12-14 13:02:28 +0700

    update nullable for file in Room Type

--- IDBMS_API/Services/RoomTypeService.cs
+++ IDBMS_API/Services/RoomTypeService.cs
@@ -46,20 +46,31 @@ namespace IDBMS_API.Services
         }
         public async Task<RoomType?> CreateRoomType([FromForm] RoomTypeRequest roomType)
         {
-            FirebaseService s = new FirebaseService();
-            string link = await s.UploadImage(roomType.IconImage);
-            string link2 = await s.UploadImage(roomType.Image);
             var rt = new RoomType
             {
                 Name = roomType.Name,
                 EnglishName = roomType.EnglishName,
-                ImageUrl = link2,
                 Description = roomType.Description,
                 EnglishDescription = roomType.EnglishDescription,
                 PricePerArea = roomType.PricePerArea,
                 IsHidden = false,
-                IconImageUrl = link,
             };
+
+            if (roomType.Image != null)
+            {
+                FirebaseService s = new FirebaseService();
+                string link = await s.UploadImage(roomType.Image);
+
+                rt.ImageUrl = link;
+            }
+
+            if (roomType.IconImage != null)
+            {
+                FirebaseService s = new FirebaseService();
+                string link = await s.UploadImage(roomType.IconImage);
+
+                rt.IconImageUrl = link;
+            }
 
             var roomTypeCreated = _repository.Save(rt);
             return roomTypeCreated;
@@ -67,16 +78,28 @@ namespace IDBMS_API.Services
         public async void UpdateRoomType(int id, [FromForm] RoomTypeRequest roomType)
         {
             var rt = _repository.GetById(id) ?? throw new Exception("This object is not existed!");
-            FirebaseService s = new FirebaseService();
-            string link = await s.UploadImage(roomType.IconImage);
-            string link2 = await s.UploadImage(roomType.Image);
+
+            if (roomType.IconImage!= null)
+            {
+                FirebaseService s = new FirebaseService();
+                string link = await s.UploadImage(roomType.IconImage);
+
+                rt.IconImageUrl = link;
+            }
+
+            if (roomType.Image != null)
+            {
+                FirebaseService s = new FirebaseService();
+                string link = await s.UploadImage(roomType.Image);
+
+                rt.ImageUrl = link;
+            }
+
             rt.Name = roomType.Name;
             rt.EnglishName = roomType.EnglishName;
-            rt.ImageUrl = link2;
             rt.Description = roomType.Description;
             rt.EnglishDescription = roomType.EnglishDescription;
             rt.PricePerArea = roomType.PricePerArea;
-            rt.IconImageUrl = link;
 
             _repository.Update(rt);
         }
