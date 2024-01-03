48f96e0375d30f169dec26da97c703a52244842b
tkiet1808 <kietpttse161220@fpt.edu.vn>
2024-01-03 21:53:46 +0700

    update download controller

--- IDBMS_API/Controllers/IDBMSControllers/DownloadController.cs
+++ IDBMS_API/Controllers/IDBMSControllers/DownloadController.cs
@@ -1,5 +1,6 @@
 ﻿using API.Supporters.JwtAuthSupport;
 using BLL.Services;
+using IDBMS_API.DTOs.Response;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IDBMS_API.Controllers.IDBMSControllers
@@ -15,12 +16,31 @@ namespace IDBMS_API.Controllers.IDBMSControllers
         }
         [HttpGet]
         [Authorize]
-        public async Task<IActionResult> Download(string url)
+        public async Task<DownloadResponse> Download(string url, string? name)
         {
             var content = await firebaseService.DownloadFileByDownloadUrl(url);
             string before = url.Split("?alt=")[0];
-            string fileName = before.Substring(before.LastIndexOf("%2F")+3);
-            return File(content, "application/octet-stream",fileName);
+
+            string fileType = Path.GetExtension(before);
+            string fileName = "";
+
+            if (name != null)
+            {
+                fileName = $"{name}{fileType}";
+            }
+            else
+            {
+                fileName = before.Substring(before.LastIndexOf("%2F") + 3);
+            }
+
+            var response = new DownloadResponse
+            {
+                FileName = fileName,
+                FileType = fileType,
+                File = File(content, "application/octet-stream", fileName),
+            };
+
+            return response;
         }
     }
 }
---
+++ IDBMS_API/DTOs/Response/DownloadResponse.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IDBMS_API.DTOs.Response
+{
+    public class DownloadResponse
+    {
+        public string FileName { get; set; } = default!;
+        public string FileType { get; set; } = default!;
+        public FileContentResult File { get; set; } = default!;
+    }
+}
