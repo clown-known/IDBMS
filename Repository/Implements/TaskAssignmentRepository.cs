a34781d2ec9e2d4653674a2b0fe721e42ac41364
tkiet1808 <kietpttse161220@fpt.edu.vn>
2024-01-12 19:58:07 +0700

    add get task assignment by task id API

--- Repository/Implements/TaskAssignmentRepository.cs
+++ Repository/Implements/TaskAssignmentRepository.cs
@@ -82,6 +82,7 @@ namespace Repository.Implements
                 using var context = new IdtDbContext();
                 return context.TaskAssignments
                     .Include(ta => ta.ProjectParticipation)
+                        .ThenInclude(ta=> ta.User)
                     .Where(ta => ta.ProjectTaskId == id)
                     .OrderByDescending(a => a.CreatedDate)
                     .ToList();
